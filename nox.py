a96b951ba3d556ec7832eeaefacf879eae466d92
Danny Hermes <daniel.j.hermes@gmail.com>
2018-06-29 13:57:50 -0700

    A "braindump" commit of the thesis template in use.
    This uses `biber` rather than `bibtex` for now since the example [1]
    uses `biber` (I plan on modifying back to use `bibtex`).
    
    [1]: https://www.sharelatex.com/templates/thesis/university-of-california-berkeley-thesis

---
+++ nox.py
@@ -0,0 +1,101 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This is a configuration file for running ``nox`` on this project.
+
+To determine the supported actions run ``nox --list-sessions`` from the
+project root.
+"""
+
+import os
+
+import nox
+import py.path
+
+
+NOX_DIR = os.path.abspath(os.path.dirname(__file__))
+SINGLE_INTERP = "python3.6"
+
+
+def get_path(*names):
+    return os.path.join(NOX_DIR, *names)
+
+
+class Remove(object):
+    def __init__(self, prefix, extensions):
+        self.prefix = prefix
+        self.extensions = extensions
+
+    def __call__(self):
+        for extension in self.extensions:
+            path = "{}.{}".format(self.prefix, extension)
+            os.remove(path)
+
+
+def build_tex_file(session, base, new_id, extensions=(), with_bibtex=False):
+    # NOTE: This assumes that ``session.chdir(get_path('doc'))``
+    #       has been called.
+    modify_id = get_path("scripts", "modify_pdf_id.py")
+
+    if with_bibtex:
+        session.run("pdflatex", base)
+        session.run("biber", base)
+        session.run("pdflatex", base)
+        session.run("biber", base)
+        session.run("pdflatex", base)
+        session.run("pdflatex", base)
+    else:
+        session.run("pdflatex", base)
+        session.run("pdflatex", base)
+        session.run("pdflatex", base)
+
+    path = get_path("doc", base)
+    remove = Remove(path, extensions)
+    session.run(remove)
+    session.run("python", modify_id, "--base", path, "--id", new_id)
+
+
+@nox.session
+def build_tex(session):
+    session.interpreter = SINGLE_INTERP
+
+    if py.path.local.sysfind("pdflatex") is None:
+        session.skip("`pdflatex` must be installed")
+
+    if py.path.local.sysfind("bibtex") is None:
+        session.skip("`bibtex` must be installed")
+
+    # No need to create a virtualenv.
+    session.virtualenv = False
+
+    session.chdir(get_path("doc"))
+
+    build_tex_file(
+        session,
+        "thesis",
+        "55008F4EDC13ADFCEFED89CA0A359ACD",
+        extensions=(
+            "aux",
+            "bbl",
+            "bcf",
+            "blg",
+            "lof",
+            "log",
+            "lot",
+            "run.xml",
+            "toc",
+        ),
+        with_bibtex=True,
+    )
+    session.run(Remove("abstract", ("aux",)))
+    session.run(Remove("chapter1", ("aux",)))
+    session.run(Remove("chapter2", ("aux",)))
