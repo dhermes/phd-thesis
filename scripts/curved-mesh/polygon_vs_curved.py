48c5f88bfc974940c65370c64657f41f9f083dc2
Danny Hermes <daniel.j.hermes@gmail.com>
2018-07-13 00:12:38 -0700

    Follow-up to previous commit.
    A similar example showing the area of bezier triangle
    intersection (as compared to the area of a single
    bezier triangle).

--- scripts/curved-mesh/polygon_vs_curved.py
+++ scripts/curved-mesh/polygon_vs_curved.py
@@ -43,14 +43,24 @@ def shoelace_for_area(nodes):
     return result / scale_factor
 
 
-def compute_area(surface):
+def compute_area(*edges):
     area = 0.0
-    for edge in surface.edges:
+    for edge in edges:
         area += shoelace_for_area(edge.nodes)
     return area
 
 
-def main():
+def edges_to_polygon(edge1, edge2, edge3, s_vals):
+    # NOTE: This assumes ``s_vals[0] == 0.0`` and ``s_vals[-1] < 1.0``.
+    N = len(s_vals)
+    polygon_nodes = np.empty((2, 3 * N), order="F")
+    polygon_nodes[:, :N] = edge1.evaluate_multi(s_vals)
+    polygon_nodes[:, N : 2 * N] = edge2.evaluate_multi(s_vals)
+    polygon_nodes[:, 2 * N :] = edge3.evaluate_multi(s_vals)
+    return polygon_nodes
+
+
+def bezier_triangle_area():
     nodes = np.asfortranarray(
         [
             [0.0, 0.5, 1.0, 0.125, 0.375, 0.25],
@@ -59,21 +69,18 @@ def main():
     )
     surface = bezier.Surface.from_nodes(nodes)
     exact_area = F(37, 96)
+    edge1, edge2, edge3 = surface.edges
 
     figure, all_axes = plt.subplots(2, 3)
     all_axes = all_axes.flatten()
     surface.plot(256, ax=all_axes[0])
     all_axes[0].set_title("Curved")
 
-    edge1, edge2, edge3 = surface.edges
     error_vals = []
     for n in range(1, 20 + 1):
         N = 2 ** n
         s_vals = np.linspace(0.0, 1.0, N + 1)[:-1]
-        polygon_nodes = np.empty((2, 3 * N), order="F")
-        polygon_nodes[:, :N] = edge1.evaluate_multi(s_vals)
-        polygon_nodes[:, N : 2 * N] = edge2.evaluate_multi(s_vals)
-        polygon_nodes[:, 2 * N :] = edge3.evaluate_multi(s_vals)
+        polygon_nodes = edges_to_polygon(edge1, edge2, edge3, s_vals)
         polygon = shapely.geometry.Polygon(polygon_nodes.T)
         # Compute the relative error.
         poly_area = F(polygon.area)
@@ -115,7 +122,7 @@ def main():
         markersize=5,
         label="Polygonal",
     )
-    surface_area = F(compute_area(surface))
+    surface_area = F(compute_area(edge1, edge2, edge3))
     curved_rel_error = float(abs(exact_area - surface_area) / exact_area)
     ax.loglog(
         [error_vals[0, 0], error_vals[-1, 0]],
@@ -141,6 +148,142 @@ def main():
     figure.savefig(path, bbox_inches="tight")
     print("Saved {}".format(filename))
     plt.close(figure)
+
+
+def intersection_area():
+    # NOTE: These are surfaces 1Q and 2Q from the ``bezier`` project.
+    nodes1 = np.asfortranarray(
+        [
+            [-0.25, 0.1875, 0.625, -0.25, 0.1875, -0.25],
+            [-0.25, -0.25, -0.25, 0.1875, 0.1875, 0.625],
+        ]
+    )
+    surface1 = bezier.Surface.from_nodes(nodes1)
+    edge11, edge12, edge13 = surface1.edges
+    nodes2 = np.asfortranarray(
+        [
+            [0.0, 0.5, 1.0, 0.0625, 0.4375, -0.125],
+            [-1.0, -0.5, 0.0, -0.5, 0.0, 0.0],
+        ]
+    )
+    surface2 = bezier.Surface.from_nodes(nodes2)
+    edge21, edge22, edge23 = surface2.edges
+
+    exact_area = F(7, 48)
+
+    figure, all_axes = plt.subplots(2, 3)
+    all_axes = all_axes.flatten()
+
+    ax = all_axes[0]
+    intersection, = surface1.intersect(surface2)
+    surface1.plot(256, ax=ax)
+    surface2.plot(256, ax=ax)
+    intersection.plot(256, ax=ax)
+    ax.set_title("Curved")
+
+    error_vals = []
+    for n in range(1, 20 + 1):
+        N = 2 ** n
+        s_vals = np.linspace(0.0, 1.0, N + 1)[:-1]
+        polygon_nodes1 = edges_to_polygon(edge11, edge12, edge13, s_vals)
+        polygon1 = shapely.geometry.Polygon(polygon_nodes1.T)
+        polygon_nodes2 = edges_to_polygon(edge21, edge22, edge23, s_vals)
+        polygon2 = shapely.geometry.Polygon(polygon_nodes2.T)
+
+        poly_intersect = polygon1.intersection(polygon2)
+        if not isinstance(poly_intersect, shapely.geometry.Polygon):
+            raise TypeError(poly_intersect)
+        # Compute the relative error.
+        approximate_area = F(poly_intersect.area)
+        rel_error = abs(approximate_area - exact_area) / exact_area
+        error_vals.append((N, float(rel_error)))
+        # Plot the "approximate" surfaces as well as their
+        # polygonal intersection.
+        if n in (1, 2, 3, 4):
+            ax = all_axes[n]
+            for polygon_nodes, color in (
+                (polygon_nodes1, plot_utils.BLUE),
+                (polygon_nodes2, plot_utils.GREEN),
+            ):
+                # Wrap-around the first node so the polygon is closed.
+                polygon_nodes = np.hstack(
+                    [polygon_nodes, polygon_nodes[:, :1]]
+                )
+                patch = matplotlib.patches.PathPatch(
+                    matplotlib.path.Path(polygon_nodes.T),
+                    alpha=0.625,
+                    color=color,
+                )
+                ax.add_patch(patch)
+                ax.plot(
+                    polygon_nodes[0, :],
+                    polygon_nodes[1, :],
+                    color=color,
+                    marker="o",
+                    markersize=5,
+                )
+            boundary_x, boundary_y = poly_intersect.exterior.coords.xy
+            boundary = np.empty((len(boundary_x), 2))
+            boundary[:, 0] = boundary_x
+            boundary[:, 1] = boundary_y
+            patch = matplotlib.patches.PathPatch(
+                matplotlib.path.Path(boundary),
+                alpha=0.625,
+                color=plot_utils.RED,
+            )
+            ax.add_patch(patch)
+            ax.set_title("$N = {:d}$".format(N))
+
+    for ax in all_axes[:5]:
+        ax.axis("equal")
+        ax.set_xlim(-0.3125, 1.0625)
+        ax.set_ylim(-1.08125, 0.70625)
+    for ax in all_axes[:3]:
+        ax.set_xticklabels([])
+    for ax in all_axes[(1, 2, 4),]:
+        ax.set_yticklabels([])
+
+    error_vals = np.array(error_vals)
+    ax = all_axes[5]
+    ax.loglog(
+        error_vals[:, 0],
+        error_vals[:, 1],
+        basex=2,
+        marker="o",
+        markersize=5,
+        label="Polygonal",
+    )
+    curved_poly_area = F(compute_area(*intersection._edges))
+    curved_rel_error = float(abs(exact_area - curved_poly_area) / exact_area)
+    ax.loglog(
+        [error_vals[0, 0], error_vals[-1, 0]],
+        [curved_rel_error, curved_rel_error],
+        basex=2,
+        color="black",
+        linestyle="dashed",
+        label="Curved",
+    )
+    ax.legend()
+    ax.set_title("Intersection Area Estimates")
+    ax.set_xlabel("Line Segments per Side ($N$)")
+    ax.set_ylabel("Relative Error")
+    ax.yaxis.tick_right()
+    ax.yaxis.set_label_position("right")
+
+    figure.set_size_inches(12.02, 5.6)
+    figure.subplots_adjust(
+        left=0.04, bottom=0.05, right=0.94, top=0.95, wspace=0.03, hspace=0.13
+    )
+    filename = "polygon_vs_curved_intersection.pdf"
+    path = plot_utils.get_path("curved-mesh", filename)
+    figure.savefig(path, bbox_inches="tight")
+    print("Saved {}".format(filename))
+    plt.close(figure)
+
+
+def main():
+    bezier_triangle_area()
+    intersection_area()
 
 
 if __name__ == "__main__":
